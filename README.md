d06f8f6a05ca1804f944ee63befc6c557659cad5
Kang-Kyu Lee <kangkyu1111@gmail.com>
2017-11-25 14:54:18 -0800

    Select percentage with plus and minus button

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ a beginner challenge / first project
 TODO:
 
 + ~~set currency (dollar) format for input field and text view~~
-+ add menu bar to select percentage options (15%, 18%, 20%, &c)
++ ~~add menu bar to select percentage options (15%, 18%, 20%, &c)~~
 + better layout and colors (button, background, text)
 + issue: empty string input crashes
 + use BigDecimal for big bills (?)
--- app/src/main/java/com/kangkyu/calculatetip/MainActivity.java
+++ app/src/main/java/com/kangkyu/calculatetip/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+    int mPercentage = 15; // tipping 15% as initial value.
     private EditText inputField;
     private Button submitButton;
 
@@ -31,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
                         String subTotal = inputField.getText().toString();
 
                         // Toast.makeText(MainActivity.this, subTotal, Toast.LENGTH_LONG).show();
-                        double percentage = 0.15;  // tipping 15%
+                        double percentage = mPercentage * 1.0 / 100;
 
                         result.setText(NumberFormat.getCurrencyInstance().format(Float.valueOf(subTotal) * (1 + percentage)));
                         tipAmount.setText(NumberFormat.getCurrencyInstance().format(Float.valueOf(subTotal) * percentage));
@@ -40,4 +41,29 @@ public class MainActivity extends AppCompatActivity {
         );
     }
 
+    /**
+     * This method is called when the + button is clicked.
+     */
+    public void increment(View view) {
+        mPercentage = mPercentage + 1;
+        displayPercentage(mPercentage);
+    }
+
+    /**
+     * This method is called when the - button is clicked.
+     */
+    public void decrement(View view) {
+        mPercentage = mPercentage - 1;
+        displayPercentage(mPercentage);
+    }
+
+    /**
+     * This method displays tip percentage with % sign on the screen.
+     *
+     * @param number percentage value as integer
+     */
+    private void displayPercentage(int number) {
+        TextView percentTextView = (TextView) findViewById(R.id.percentage_text_view);
+        percentTextView.setText(number + "%");
+    }
 }
